1a263622147a327d3c6febd7864531067fc63839
Lucia Pintor <luciapintor90@gmail.com>
2024-03-13 16:54:06 +0100

    added - report creation

--- src/calculate_scores.py
+++ src/calculate_scores.py
@@ -1,21 +1,33 @@
 # by Lucia Pintor
+import pandas as pd
+
 from src.capture_score import CaptureScore
 from src.get_data import get_data_from_captures
 from src.get_ground_truth import get_ground_truth
+from src.utils.create_missing_folder import create_missing_folder
 
 
-def calculate_scores(ground_truth, participant_summary):
+def calculate_scores(ground_truth, participant_summary, participant_name):
     """
     This function iterates the three output files of each participant to calculate their scores.
     :param ground_truth:
     :param participant_summary:
+    :param participant_name:
     :return:
     """
     scores = []
+    scores_dict = []
 
     for i, c in enumerate(["A", "B", "C"]):
-        scores.append(
-            calculate_score_single_capture(capture_id=c, capture_gt=ground_truth[i], capture_ps=participant_summary[i]))
+        single_capture_score = calculate_score_single_capture(capture_id=c, capture_gt=ground_truth[i],
+                                                              capture_ps=participant_summary[i])
+        scores.append(single_capture_score)
+        scores_dict.append(single_capture_score.__dict__)
+
+    # save as csv
+    scores_df = pd.DataFrame(scores_dict)
+    report_folder = create_missing_folder("data/reports/")
+    scores_df.to_csv(report_folder + participant_name + ".csv")
 
     return scores
 
@@ -48,11 +60,12 @@ def calculate_score_single_capture(capture_id, capture_gt, capture_ps):
 if __name__ == "__main__":
     ground_truth = get_ground_truth()
 
-    data_folder = "data/participants/example/"
+    data_folder = "data/participants/"
+    participant_name = "example"
 
-    participant_summary = get_data_from_captures(data_folder)
+    participant_summary = get_data_from_captures(data_folder + participant_name + "/")
 
-    scores = calculate_scores(ground_truth, participant_summary)
+    scores = calculate_scores(ground_truth, participant_summary, participant_name)
 
     for score in scores:
         print(score.score)
